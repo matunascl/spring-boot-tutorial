5cd79c380b43b12e3b1739257a313cc0f45756f7
matunascl <matunascl@gmail.com>
2021-08-27 14:55:23 -0400

    added to database

---
+++ demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    1L,
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21);
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5),
+                    21);
+
+
+            repository.saveAll(List.of(mariam, alex));
+        };
+    }
+}
--- demo/demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +10,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21)
-        );
+        return studentRepository.findAll();
     }
 
 }
